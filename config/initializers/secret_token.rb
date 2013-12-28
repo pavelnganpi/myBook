2b63d15ccff8b58bf0ef081cfeee0aa9b05f0ad1
paveytel <paveytel@buffalo.edu>
2013-12-27 23:55:43 -0500

    created a mybook application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mybook::Application.config.secret_key_base = 'a2563a7b9c9d34926dea2e914208c769c806928a39f4734f2fd8631e4547995ab73573ccdd71a1cde002cd4669ccbc35f43aeee79aae369dbb18ed7267d665e2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mybook::Application.config.session_store :cookie_store, key: '_mybook_session'
